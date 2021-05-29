941f81d6dcf9d7b375d4b58294163e44a6232724
Ben Lu <sihao@squareup.com>
2021-05-28 17:01:56 -0700

    Initial Commit

---
+++ Shared/SatelliteForcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatelliteForcastApp.swift
+//  Shared
+//
+//  Created by Ben Lu on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SatelliteForcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
