e0c6c38d736a7e5da1fdb2e3a17538211773e9ea
Sihao Lu <lsh32768@gmail.com>
2022-04-01 01:50:01 -0700

    Add headers for settings tab

--- SatelliteForecast/Views/SettingsOverview/SettingsOverviewView.swift
+++ SatelliteForecast/Views/SettingsOverview/SettingsOverviewView.swift
@@ -46,33 +46,66 @@ struct SettingsOverviewViewImpl: SettingsOverviewView {
         }
     }
 
+    @ViewBuilder private func sectionHeader(for item: SettingsOverviewItem) -> some View {
+        switch item {
+        case .alarms:
+            Text(
+                NSLocalizedString(
+                    "SettingsOverviewView.alarms.header",
+                    tableName: nil,
+                    bundle: .main,
+                    value: "Alarms",
+                    comment: "The section header of the alarms section in settings"
+                )
+            )
+            .font(.headline.lowercaseSmallCaps().weight(.semibold))
+            .foregroundColor(Color(UIColor.secondaryLabel))
+        case .observer:
+            Text(
+                NSLocalizedString(
+                    "SettingsOverviewView.observer.header",
+                    tableName: nil,
+                    bundle: .main,
+                    value: "Location settings",
+                    comment: "The section header of the observer section in settings"
+                )
+            )
+            .font(.headline.lowercaseSmallCaps().weight(.semibold))
+            .foregroundColor(Color(UIColor.secondaryLabel))
+        }
+    }
+
     @ViewBuilder private func navigationLink(for item: SettingsOverviewItem) -> some View {
-        NavigationLink(
-            destination: LazyView(destination(for: item)),
-            tag: item,
-            selection: Binding<SettingsOverviewItem?>(
-                get: {
-                    if viewModel.state.alarmNavigation.enabled {
-                        return .alarms
-                    } else if viewModel.state.observerNavigation.enabled {
-                        return .observer
-                    } else {
-                        return nil
+        Section {
+            NavigationLink(
+                destination: LazyView(destination(for: item)),
+                tag: item,
+                selection: Binding<SettingsOverviewItem?>(
+                    get: {
+                        if viewModel.state.alarmNavigation.enabled {
+                            return .alarms
+                        } else if viewModel.state.observerNavigation.enabled {
+                            return .observer
+                        } else {
+                            return nil
+                        }
+                    },
+                    set: { item, _ in
+                        viewModel.dispatch(.selectSettingItem(item))
                     }
-                },
-                set: { item, _ in
-                    viewModel.dispatch(.selectSettingItem(item))
+                ),
+                label: {
+                    switch item {
+                    case .observer:
+                        observerCellViewProducer.view()
+                    case .alarms:
+                        alarmSettingsCellProducer.view()
+                    }
                 }
-            ),
-            label: {
-                switch item {
-                case .observer:
-                    observerCellViewProducer.view()
-                case .alarms:
-                    alarmSettingsCellProducer.view()
-                }
-            }
-        )
+            )
+        } header: {
+            sectionHeader(for: item)
+        }
     }
 
     var body: some View {
