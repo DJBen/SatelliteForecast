54a74dc78c69ba93c56c6b28bb6c522224e81b3d
Ben Lu <sihao@squareup.com>
2022-11-13 01:45:58 -0800

    Fixed a crash caused by missing first element in localization interpolation

--- Frameworks/SatelliteForecast/Impl/Sources/Views/PassView/PassView.swift
+++ Frameworks/SatelliteForecast/Impl/Sources/Views/PassView/PassView.swift
@@ -233,7 +233,7 @@ extension PassView {
             "PassView.descriptionToolbar.text",
             tableName: nil,
             bundle: .main,
-            value: "Rises from %2$@ and sets into %3$@",
+            value: "Rises from %1$@ and sets into %2$@",
             comment: "The toolbar of the pass view describing the direction of the pass. The first and second arguments correspond to the directions of rising and setting."
         )
         return String(format: format, riseDirection, setDirection)
