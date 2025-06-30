a7e10668964798c0fd05acd61a08d619cff85057
Sihao Lu <lsh32768@gmail.com>
2025-06-30 03:13:00 -0700

    Enable deep link into ISS/Tianhe from notifications

--- Frameworks/SatelliteForecast/Public/Sources/Primitives/SatelliteCategory.swift
+++ Frameworks/SatelliteForecast/Public/Sources/Primitives/SatelliteCategory.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum SatelliteCategory: Equatable, Hashable, Codable, Sendable {
+public enum SatelliteCategory: String, Equatable, Hashable, Codable, Sendable {
     /// International Space Station: 25544
     case iss
     
